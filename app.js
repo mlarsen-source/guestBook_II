2afa60e764c77ee7185f24d781d2eb0c4733dc3d
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-02-05 22:05:43 -0800

    added admin route that displays the data for all form submissions.

--- app.js
+++ app.js
@@ -8,22 +8,25 @@ File Description: JavaScript for Guestbook app
 
 */
 
-
+// import express module
 import express from 'express';
+
+// instantiate an express application
 const app = express();
+
+// instantiate an array to store form submissions
 const contactList = [];
 
-
+// serve static files from the 'public' directory
 app.use(express.static('public'));
 
-
+// handling form submission for data sent in the URL-encoded format
 app.use(express.urlencoded({extended: true}));
 
 // default route for contact form home page
 app.get('/',(req, res)=>{
 
   res.sendFile(`${import.meta.dirname}/views/home.html`);
-
 });
 
 
@@ -33,8 +36,8 @@ app.post('/confirm', (req, res) =>
   // Get form data from request body
   const contact = 
   {
-    fname: req.body.fname,
     lname: req.body.lname,
+    fname: req.body.fname,
     title: req.body.title,
     company: req.body.company,
     linkedIn: req.body.linkedIn,
@@ -45,36 +48,33 @@ app.post('/confirm', (req, res) =>
     format: req.body.format,
     timestamp: new Date()
   };
-  
   // Save contact info to the contactList array
   contactList.push(contact)
   
   // Log the contactList array to the console
   console.log(req.body);
-
-
-      //create dynamic confirmation page
-  //let confirmationHTML = '<h2>Thank you.</h2><p>The following appointment has been scheduled.</p><ul>';
-  //confirmationHTML += `<li>Name: ${appointment.fname} ${appointment.lname} </li><li>Date:  ${appointment.date}</li><li>Time: ${appointment.time} </li>`;
-  //confirmationHTML += '</ul>';
-  
+ 
   // send confirmation page to user
   res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+});
+
+// admin route to view all contacts
+app.get('/admin/contacts', (req, res) => {
   
+  // create dynamic admin page
+  let html = '<h1>Contact List</h1><ul>';
+  for (const contacts of contactList) 
+  {
+    html += `<li>${contacts.lname} ${contacts.fname} - ${contacts.title} - ${contacts.company} - ${contacts.linkedIn} - ${contacts.meet} -
+    ${contacts.other} - ${contacts.message} - ${contacts.mailList} - ${contacts.format} - ${contacts.timestamp}</li>`;
+  }
+  html += '</ul>';
+  res.send(html);
 });
 
 
-
-
-//app.get('/confirm',(req, res)=>{
-
-  //res.sendFile(`${import.meta.dirname}/views/confirm.html`);
-
-//});
-
-
+// tell the server to listen on port 3000
 app.listen(3000, ()=>{
 
   console.log("Server running at http://localhost:3000")
-
 });
